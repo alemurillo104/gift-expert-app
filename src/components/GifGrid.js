9fee87ae2f8871332df2f5d82ba6d5b74a6d4b39
Alejandra Murillo <alemurillo104@gmail.com>
2021-10-01 21:19:51 -0400

    Fin de la seccion 6

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -6,23 +6,13 @@ import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    // useEffect(() => {
-        
-    //     getGifs( category ).then( setImages);
-    //     // return async () => {
-    //     //     const resp = await getGifs( category );
-    //     //     setImages( resp ); 
-    //     // }
-    // }, [ category ])
-
-
     const { data: images, loading } = useFetchGifs( category );
 
     return (
         <>
-            <h3>{category}</h3>
+            <h3 className=" animate__animated animate__fadeInLeft">{category}</h3>
 
-            { loading && <p>Loading...</p>}
+            { loading && <p className="animate__animated animate__flash">Loading...</p>}
 
             <div className="card-grid">
                 {
